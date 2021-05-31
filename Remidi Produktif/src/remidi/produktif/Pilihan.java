224a8c9662eb79aa049f6f7478f0810077c11d8f
arifin-ilham <ilhamku2005@gmail.com>
2021-05-31 15:16:29 +0700

    Remidi Produktif 1

---
+++ Remidi Produktif/src/remidi/produktif/Pilihan.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package remidi.produktif;
+
+/**
+ *
+ * @author ilham
+ */
+public class Pilihan extends JayaBaru{
+    
+    public Pilihan(){
+        System.out.println("Toko Jaya Baru");
+        System.out.println("==============");
+  
+    }
+}
+
+  
